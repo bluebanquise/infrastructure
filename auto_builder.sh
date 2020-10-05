0d92243bcfcc750de7f0b55ff467b33242f96fbc
oxedions <oxedions@gmail.com>
2020-10-05 15:15:17 +0200

    Fix: Add missing prometheus_client package

--- auto_builder.sh
+++ auto_builder.sh
@@ -114,10 +114,23 @@ case $value in
 
     2) ######################################################################################
         set -x
+        if [ ! -f $working_directory/sources/prometheus_client-$prometheus_client_version.tar.gz ]; then
+            wget -P $working_directory/sources/ https://github.com/prometheus/client_python/archive/v$prometheus_client_version.tar.gz
+            mv $working_directory/sources/v$prometheus_client_version.tar.gz $working_directory/sources/prometheus_client-$prometheus_client_version.tar.gz
+        fi
+        rm -Rf $working_directory/build/prometheus
+        mkdir -p $working_directory/build/prometheus
+        cd $working_directory/build/prometheus
+        cp $working_directory/sources/prometheus_client-$prometheus_client_version.tar.gz .
+        tar xvzf prometheus_client-$prometheus_client_version.tar.gz
+        cd client_python-$prometheus_client_version
+        python setup.py bdist_rpm --spec-only
+        cd ..
+        mv client_python-$prometheus_client_version prometheus_client-$prometheus_client_version
+        tar cvzf prometheus_client-$prometheus_client_version.tar.gz prometheus_client-$prometheus_client_version
+        rpmbuild -ta prometheus_client-$prometheus_client_version.tar.gz
+
         if [ $distribution_architecture == 'x86_64' ]; then
-            rm -Rf $working_directory/build/prometheus
-            mkdir -p $working_directory/build/prometheus
-            cd $working_directory/build/prometheus
 
             cp -a $root_directory/packages/prometheus $working_directory/build/prometheus/prometheus
             mv prometheus prometheus-$prometheus_version
