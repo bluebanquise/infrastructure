97327ac2184868a102044699f6814e3cab3c6788
oxedions <oxedions@gmail.com>
2021-10-13 15:14:47 +0200

    up

--- packages/prometheus/version.sh
+++ packages/prometheus/version.sh
@@ -1,8 +1,7 @@
-prometheus_version=2.20.1
-prometheus_client_version=0.8.0
-alertmanager_version=0.21.0
-ipmi_exporter_version=1.3.1
-node_exporter_version=1.0.1
-karma_version=0.68
-snmp_exporter_version=0.18.0
-
+prometheus_version=2.30.3
+prometheus_client_version=0.11.0
+alertmanager_version=0.23.0
+ipmi_exporter_version=1.4.0
+node_exporter_version=1.2.2
+karma_version=0.92
+snmp_exporter_version=0.20.0
