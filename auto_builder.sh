14592413ac5511437382ec3656d489caf9fa6b76
oxedions <oxedions@gmail.com>
2021-10-13 15:35:05 +0200

    up

--- auto_builder.sh
+++ auto_builder.sh
@@ -204,29 +204,7 @@ case $value in
     ;;
 
     5) ######################################################################################
-        set -x
-
-        if [ $distribution_version -eq 7 ]; then
-            ln -s /usr/bin/aclocal /usr/bin/aclocal-1.16
-            ln -s /usr/bin/autoconf /usr/bin/autoconf-1.16
-            ln -s /usr/bin/automake /usr/bin/automake-1.16
-        fi
-        rm -Rf $working_directory/build/atftp
-        mkdir -p $working_directory/build/atftp/
-
-        if [ ! -f $working_directory/sources/atftp-0.7.2.tar.gz ]; then
-            wget -P $working_directory/sources/ https://freefr.dl.sourceforge.net/project/atftp/atftp-0.7.2.tar.gz
-        fi
-
-        cd $working_directory/build/atftp/
-        cp $working_directory/sources/atftp-0.7.2.tar.gz $working_directory/build/atftp/
-        tar xvzf atftp-0.7.2.tar.gz
-        /usr/bin/cp -f $root_directory/packages/atftp/* atftp-0.7.2/
-        rm -f atftp-0.7.2/redhat/atftp.spec
-        tar cvzf atftp.tar.gz atftp-0.7.2
-        rpmbuild -ta atftp.tar.gz
-
-        set +x
+        source $root_directory/packages/atftp/build.sh
     ;;
 
     6) ######################################################################################
