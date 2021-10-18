39784f9fee2ff27a7e2be2d1ce9544b922dfde2e
oxedions <oxedions@gmail.com>
2021-10-18 21:36:21 +0200

    up

--- auto_builder.sh
+++ auto_builder.sh
@@ -92,14 +92,14 @@ case $value in
     0) ######################################################################################
         set -x
         echo " Installing needed packages... may take some time."
-        if [ "$distribution" == 'openSUSE Leap' ]; then
-          if [ "$distribution_version" == "15.1" ]; then
+        if [ "$distribution" == 'opensuse_leap' ]; then
+          if [ "$distribution_version" == "15.3" ]; then
             zypper -n install gcc rpm-build make mkisofs xz xz-devel automake autoconf bzip2 openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2 grub2-x86_64-efi mariadb munge munge-devel freeipmi freeipmi-devel  mariadb mariadb-client libmariadb-devel libmariadb3
           fi
 	      elif [ "$distribution" == 'Ubuntu' ]; then
            apt-get update
            export DEBIAN_FRONTEND=noninteractive
-	         apt-get install -y liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget
+	         apt-get install -y bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget
 	         # Possibly missing python3-mysqldb libmysqld-dev
         elif [ "$distribution" == 'RedHat' ]; then
           if [ $distribution_version -eq 8 ]; then
