7ad89493b2a15f29bb1f9cdc829b743db5ccfef5
oxedions <oxedions@gmail.com>
2021-10-20 21:33:21 +0200

    up

--- auto_builder.sh
+++ auto_builder.sh
@@ -113,10 +113,12 @@ case $value in
 #              alternatives --set python /usr/bin/python3
             fi
             if [ $distribution_architecture == 'aarch64' ]; then
-              dnf install 'dnf-command(config-manager)'
-              dnf install make rpm-build genisoimage xz xz-devel automake autoconf python36 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-aa64-modules gcc mariadb mariadb-devel curl-devel net-snmp-devel dnf-plugins-core -y
+              dnf install 'dnf-command(config-manager)' -y
+	            dnf install dnf-plugins-core -y
+              dnf install make rpm-build genisoimage xz xz-devel automake autoconf python36 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-aa64-modules gcc mariadb mariadb-devel dnf-plugins-core curl-devel net-snmp-devel wget bc rsync xorriso -y
               dnf config-manager --set-enabled powertools
               dnf install freeipmi-devel -y
+	            dnf groupinstall 'Development Tools' -y
 #              alternatives --set python /usr/bin/python3
             fi
           fi
