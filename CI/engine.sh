1e404abb6af74c6429e5467a3f66e02aff68636e
oxedions <oxedions@gmail.com>
2021-10-29 21:22:07 +0200

    up

--- CI/engine.sh
+++ CI/engine.sh
@@ -1,11 +1,12 @@
 CURRENT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Assume arm64_worker and x86_64_worker both resolve
+# Assume aarch64_worker and x86_64_worker both resolve
+# Assume remote user is jenkins user
 
 mkdir ~/CI/
 mkdir -p ~/CI/{build,repositories}/{el7,el8}/{x86_64,aarch64}/
-mkdir -p ~/CI/{build,repositories}/{ubuntu2004}/{x86_64,arm64}/
+mkdir -p ~/CI/{build,repositories}/ubuntu2004/{x86_64,arm64}/
 
 # BUILDS
 
-rsync -a $CURRENT_DIR/build/RedHat_8_x86_64 jenkins@x86_64_worker:/home/jenkins/RedHat_8_x86_64
+rsync -av $CURRENT_DIR/build/RedHat_8_x86_64 jenkins@x86_64_worker:/home/jenkins/RedHat_8_x86_64
