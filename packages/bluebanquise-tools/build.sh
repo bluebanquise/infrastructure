0c09b7923fde328372642ce202d888da231d06b0
oxedions <oxedions@gmail.com>
2021-12-14 21:58:48 +0000

    Fix: tools - Missing Ubuntu support

--- packages/bluebanquise-tools/build.sh
+++ packages/bluebanquise-tools/build.sh
@@ -9,4 +9,11 @@ cd $working_directory/build/bluebanquise-tools
 git clone https://github.com/bluebanquise/tools.git .
 ./packages.sh
 
+if [ $distribution == "Ubuntu" ]; then
+    cd /root
+    alien --to-deb --scripts /root/rpmbuild/RPMS/$distribution_architecture/bluebanquise-*
+    mkdir -p /root/debbuild/DEBS/$distribution_architecture/
+    mv *.deb /root/debbuild/DEBS/$distribution_architecture/
+fi
+
 set +x
