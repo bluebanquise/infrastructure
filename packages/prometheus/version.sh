35485ab1207165e411880a225ec9b3debad70b34
Ox <oxedions@gmail.com>
2022-01-03 22:17:26 +0100

    Feat: Prometheus - Update versions

--- packages/prometheus/version.sh
+++ packages/prometheus/version.sh
@@ -1,7 +1,7 @@
-prometheus_version=2.31.1
+prometheus_version=2.32.1
 prometheus_client_version=0.12.0
 alertmanager_version=0.23.0
 ipmi_exporter_version=1.4.0
-node_exporter_version=1.3.0
-karma_version=0.93
+node_exporter_version=1.3.1
+karma_version=0.94
 snmp_exporter_version=0.20.0
