0d9e1e343d223f9d1cdfdf65bf8e84bbf6aa5959
oxedions <oxedions@gmail.com>
2022-12-22 02:46:52 +0000

    Dev

--- Validation/steps/04_deploy_bluebanquise_on_mgt1.sh
+++ Validation/steps/04_deploy_bluebanquise_on_mgt1.sh
@@ -7,37 +7,10 @@ if (( $STEP < 5 )); then
     ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip chmod +x online_bootstrap.sh
     ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip ./online_bootstrap.sh silent
 
-    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip /bin/sh -c 'sudo cat .ssh/authorized_keys >> /var/lib/bluebanquise/.ssh/authorized_keys'
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip 'sudo cat .ssh/authorized_keys >> /var/lib/bluebanquise/.ssh/authorized_keys'
 
     scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $CURRENT_DIR/validation bluebanquise@$mgt1_ip:/var/lib/bluebanquise/
 
-if [[ false ]]
-then
-    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $CURRENT_DIR/validation bluebanquise@$mgt1_ip:/home/bluebanquise/
-
-    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null bluebanquise@$mgt1_ip bash <<EOF
-
-git clone https://github.com/bluebanquise/bluebanquise.git
-cd bluebanquise
-cp -a ../validation resources/examples/validation
-
-sed -i bootstrap_input.sh '/SAMPLE_INVENTORY/d'
-sed -i bootstrap_input.sh '/UBUNTU_2004_ISO_URL/d'
-sed -i bootstrap_input.sh '/UBUNTU_2004_ISO/d'
-sed -i bootstrap_input.sh '/REDHAT_8_ISO_URL/d'
-sed -i bootstrap_input.sh '/REDHAT_8_ISO/d'
-
-echo "SAMPLE_INVENTORY=validation" >> bootstrap_input.sh
-echo "UBUNTU_2004_ISO_URL=http://192.168.122.1:8000/isos/ubuntu-20.04.4-live-server-amd64.iso" >> bootstrap_input.sh
-echo "UBUNTU_2004_ISO=ubuntu-20.04.4-live-server-amd64.iso" >> bootstrap_input.sh
-echo "REDHAT_8_ISO_URL=http://192.168.122.1:8000/isos/AlmaLinux-8.5-x86_64-dvd.iso" >> bootstrap_input.sh
-echo "REDHAT_8_ISO=AlmaLinux-8.5-x86_64-dvd.iso" >> bootstrap_input.sh
-
-./bootstrap.sh
-
-EOF
-
-fi
 
 fi
 if (( $STEP < 6 )); then
@@ -46,7 +19,7 @@ if (( $STEP < 6 )); then
 
 #export ANSIBLE_CONFIG=/home/bluebanquise/bluebanquise/ansible.cfg
 #cd bluebanquise/playbooks
-
+set -x
 cd validation/inventories/
 mkdir -p minimal/group_vars/all/
 cp bb_core.yml minimal/group_vars/all/
