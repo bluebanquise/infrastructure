dcec79c1de1a2fdb5efd28a895a5f2646fb92bc2
oxedions <oxedions@gmail.com>
2022-12-21 21:25:38 -0500

    Dev

--- Validation/steps/04_deploy_bluebanquise_on_mgt1.sh
+++ Validation/steps/04_deploy_bluebanquise_on_mgt1.sh
@@ -3,6 +3,16 @@ CURRENT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 
 if (( $STEP < 5 )); then
 
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip wget https://raw.githubusercontent.com/bluebanquise/bluebanquise/dev/2.0/bootstrap/online_bootstrap.sh
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip chmod +x online_bootstrap.sh
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip ./online_bootstrap.sh silent
+
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null generic@$mgt1_ip /bin/sh -c 'sudo cat .ssh/authorized_keys >> /var/lib/bluebanquise/.ssh/authorized_keys'
+
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $CURRENT_DIR/validation bluebanquise@$mgt1_ip:/var/lib/bluebanquise/
+
+if [[ false ]]
+then
     scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $CURRENT_DIR/validation bluebanquise@$mgt1_ip:/home/bluebanquise/
 
     ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null bluebanquise@$mgt1_ip bash <<EOF
@@ -28,14 +38,20 @@ echo "REDHAT_8_ISO=AlmaLinux-8.5-x86_64-dvd.iso" >> bootstrap_input.sh
 EOF
 
 fi
+
+fi
 if (( $STEP < 6 )); then
 
     ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null bluebanquise@$mgt1_ip <<EOF
 
-export ANSIBLE_CONFIG=/home/bluebanquise/bluebanquise/ansible.cfg
-cd bluebanquise/playbooks
-ansible-playbook managements.yml --limit mgt1 -b
-sudo bootset -n c[001-004],mg[2-4] -b osdeploy
+#export ANSIBLE_CONFIG=/home/bluebanquise/bluebanquise/ansible.cfg
+#cd bluebanquise/playbooks
+
+cd validation/inventories/
+mkdir -p minimal/group_vars/all/
+cp bb_core.yml minimal/group_vars/all/
+ansible-playbook ../playbooks/managements.yml -i minimal --limit mgt1 -b
+#sudo bootset -n c[001-004],mg[2-4] -b osdeploy
 
 EOF
 
