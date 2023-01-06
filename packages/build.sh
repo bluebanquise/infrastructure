a8e84efaaa527bdb184406013ba7c9388712a82d
oxedions <oxedions@gmail.com>
2023-01-06 20:42:56 +0000

    Fix: powertool to crb in el9

--- packages/build.sh
+++ packages/build.sh
@@ -140,7 +140,7 @@ if [ "$1" == "dependencies" ]; then
         dnf install 'dnf-command(config-manager)' -y
         dnf install dnf-plugins-core -y
         dnf install make rpm-build genisoimage xz xz-devel automake autoconf python3 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-x64-modules gcc mariadb dnf-plugins-core curl-devel net-snmp-devel wget bc rsync xorriso -y
-        dnf config-manager --set-enabled powertools
+        dnf config-manager --set-enabled crb
         dnf install freeipmi-devel -y
         dnf groupinstall 'Development Tools' -y
       fi
@@ -148,7 +148,7 @@ if [ "$1" == "dependencies" ]; then
         dnf install 'dnf-command(config-manager)' -y
         dnf install dnf-plugins-core -y
         dnf install make rpm-build genisoimage xz xz-devel automake autoconf python3 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-aa64-modules gcc mariadb dnf-plugins-core curl-devel net-snmp-devel wget bc rsync xorriso -y
-        dnf config-manager --set-enabled powertools
+        dnf config-manager --set-enabled crb
         dnf install freeipmi-devel -y
         dnf groupinstall 'Development Tools' -y
       fi
