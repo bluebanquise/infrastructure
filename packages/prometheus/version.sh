eff6e22bf13e8dc0b4f6682fed9fcfcce551f006
oxedions <oxedions@gmail.com>
2023-02-16 17:54:21 -0500

    Karma update

--- packages/prometheus/version.sh
+++ packages/prometheus/version.sh
@@ -3,5 +3,5 @@ prometheus_client_version=0.16.0
 alertmanager_version=0.25.0
 ipmi_exporter_version=1.6.1
 node_exporter_version=1.5.0
-karma_version=0.111
+karma_version=0.112
 #snmp_exporter_version=0.20.0
