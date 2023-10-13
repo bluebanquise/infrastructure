46034f4cb1a4721e70de1376475cbf6da8d94614
oxedions <oxedions@gmail.com>
2023-10-13 13:23:49 +0000

    Fix issues in diskless

--- diskless/RHEL_in.sh
+++ diskless/RHEL_in.sh
@@ -3,9 +3,9 @@ set -x
 source /etc/os-release
 NAME=$(echo -n $NAME | sed 's/\ /_/g')
 
-rm -Rf /tmp/image
-mkdir /tmp/image
-cd /tmp/image
+rm -Rf /image
+mkdir /image
+cd /image
 mkdir proc
 mkdir sys
 mkdir dev
@@ -14,26 +14,26 @@ mount --rbind /sys sys/
 mount --rbind /dev dev/
 
 if [[ $PLATFORM_ID == "platform:el8" ]]; then
-	dnf install -y --installroot=/tmp/image dnf vi yum iproute procps-ng openssh-server NetworkManager kernel-modules kernel dracut dracut-live nfs-utils --exclude glibc-all-langpacks --exclude grubby --exclude libxkbcommon --exclude pinentry --exclude python3-unbound --exclude unbound-libs --exclude xkeyboard-config --exclude trousers --exclude gnupg2-smime --exclude openssl-pkcs11 --exclude rpm-plugin-systemd-inhibit --exclude shared-mime-info --exclude glibc-langpack-* --setopt=module_platform_id=$PLATFORM_ID --nobest --releasever=$VERSION_ID
+	dnf install -y --installroot=/image dnf vi yum iproute procps-ng openssh-server NetworkManager kernel-modules kernel dracut dracut-live nfs-utils --exclude glibc-all-langpacks --exclude grubby --exclude libxkbcommon --exclude pinentry --exclude python3-unbound --exclude unbound-libs --exclude xkeyboard-config --exclude trousers --exclude gnupg2-smime --exclude openssl-pkcs11 --exclude rpm-plugin-systemd-inhibit --exclude shared-mime-info --exclude glibc-langpack-* --setopt=module_platform_id=$PLATFORM_ID --nobest --releasever=$VERSION_ID
 fi
 if [[ $PLATFORM_ID == "platform:el9" ]]; then
-    dnf install -y --installroot=/tmp/image dnf vi yum iproute procps-ng openssh-server NetworkManager kernel-modules kernel dracut dracut-live nfs-utils --exclude glibc-all-langpacks --exclude grubby --exclude libxkbcommon --exclude pinentry --exclude python3-unbound --exclude unbound-libs --exclude xkeyboard-config --exclude trousers --exclude gnupg2-smime --exclude openssl-pkcs11 --exclude rpm-plugin-systemd-inhibit --exclude shared-mime-info --exclude glibc-langpack-* --setopt=module_platform_id=$PLATFORM_ID --nobest --releasever=$VERSION_ID
+    dnf install -y --installroot=/image dnf vi yum iproute procps-ng openssh-server NetworkManager kernel-modules kernel dracut dracut-live nfs-utils --exclude glibc-all-langpacks --exclude grubby --exclude libxkbcommon --exclude pinentry --exclude python3-unbound --exclude unbound-libs --exclude xkeyboard-config --exclude trousers --exclude gnupg2-smime --exclude openssl-pkcs11 --exclude rpm-plugin-systemd-inhibit --exclude shared-mime-info --exclude glibc-langpack-* --setopt=module_platform_id=$PLATFORM_ID --nobest --releasever=$VERSION_ID
 fi
 
-cat << EOF > /tmp/image/etc/dracut.conf.d/bluebanquise.conf
+cat << EOF > /image/etc/dracut.conf.d/bluebanquise.conf
 add_dracutmodules+=" nfs livenet dmsquash-live "
 add_drivers+=" xfs "
 hostonly=no
 show_modules=yes
 EOF
 
-chroot /tmp/image /bin/bash -c "dracut --regenerate-all --force"
+chroot /image /bin/bash -c "dracut --regenerate-all --force"
 
 PLATFORM=$(echo ${PLATFORM_ID} | sed 's/platform://')
-KERNEL=$(ls /tmp/image/usr/lib/modules)
+KERNEL=$(ls /image/usr/lib/modules)
 KERNEL=$(echo -n ${KERNEL})
 
-cat << EOF > /tmp/image/metadata.yaml
+cat << EOF > /image/metadata.yaml
 name: ${NAME}_${VERSION_ID}
 version: ${VERSION_ID}
 family: ${PLATFORM}
@@ -46,16 +46,16 @@ url: http://bluebanquise.com/diskless/images/${NAME}_${VERSION_ID}_minimal_$(una
 packaging_date: $(date)
 EOF
 
-cd /tmp/image
-shopt -s dotglob
-tar czf /tmp/${NAME}_${VERSION_ID}_minimal_$(uname -m).tar.gz *
-chmod 777 /tmp/${NAME}_${VERSION_ID}_minimal_$(uname -m).tar.gz
-shopt -u dotglob
-
-cd /tmp/image
+cd /image
 umount proc/
 umount sys/
 umount dev/
-rm -Rf /tmp/image
+shopt -s dotglob
+tar --exclude="proc" --exclude="sys" --exclude="dev" -czf /tmp/${NAME}_${VERSION_ID}_minimal_$(uname -m).tar.gz *
+chmod 777 /tmp/${NAME}_${VERSION_ID}_minimal_$(uname -m).tar.gz
+shopt -u dotglob
+
+cd
+#rm -Rf /image
 rm -f /tmp/$1.image_link
 echo "${NAME}_${VERSION_ID}_minimal_$(uname -m).tar.gz" > /tmp/$1.image_link
--- diskless/main.sh
+++ diskless/main.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 rm -f /tmp/RHEL_in.sh
 cp -a RHEL_in.sh /tmp
-#for distrib in "almalinux:8" "rockylinux:8"
+#for distrib in "almalinux:9"
 for distrib in "almalinux:8" "rockylinux:8" "almalinux:9" "rockylinux:9"
 do
+#    sudo rm -Rf /tmp/image
     echo $distrib
     # Priviledged is mandatory for EL9 builds
     docker run --rm --privileged -v /tmp:/tmp $distrib /tmp/RHEL_in.sh $distrib
