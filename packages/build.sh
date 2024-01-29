68d1240be070290e606ac5117d594a769cab09d7
oxedions <oxedions@gmail.com>
2024-01-29 10:10:16 +0100

    Fix SCL usage

--- packages/build.sh
+++ packages/build.sh
@@ -139,7 +139,8 @@ if [ "$1" == "dependencies" ]; then
       if [ $distribution_architecture == 'x86_64' ]; then
         dnf install 'dnf-command(config-manager)' -y
         dnf install dnf-plugins-core -y
-        dnf install gcc-toolset-11-toolchain make rpm-build genisoimage xz xz-devel automake autoconf python36 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-x64-modules gcc mariadb mariadb-devel dnf-plugins-core curl-devel net-snmp-devel wget bc rsync xorriso unzip -y
+        dnf install gcc-toolset-11* -y
+        dnf install make rpm-build genisoimage xz xz-devel automake autoconf python36 bzip2-devel openssl-devel zlib-devel readline-devel pam-devel perl-ExtUtils-MakeMaker grub2-tools-extra grub2-efi-x64-modules gcc mariadb mariadb-devel dnf-plugins-core curl-devel net-snmp-devel wget bc rsync xorriso unzip -y
         dnf config-manager --set-enabled powertools
         dnf install freeipmi-devel -y
         dnf groupinstall 'Development Tools' -y
