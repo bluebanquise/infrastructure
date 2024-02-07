acf1fe0e7b36a12c459d79d39e7c0ddea2d3fdf7
oxedions <oxedions@gmail.com>
2024-02-07 15:18:16 +0100

    Typo

--- packages/build.sh
+++ packages/build.sh
@@ -78,14 +78,14 @@ if [ "$1" == "dependencies" ]; then
         echo "%_arch x86_64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
         # Possibly missing python3-mysqldb libmysqld-dev
       fi
       if [ $distribution_architecture == 'arm64' ]; then
         echo "%_arch arm64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
       fi
     fi
     if [ "$distribution_version" == "12" ]; then
@@ -93,14 +93,14 @@ if [ "$1" == "dependencies" ]; then
         echo "%_arch x86_64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
         # Possibly missing python3-mysqldb libmysqld-dev
       fi
       if [ $distribution_architecture == 'arm64' ]; then
         echo "%_arch arm64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi xz-utils unzip xorriso
       fi
     fi
 
@@ -110,14 +110,14 @@ if [ "$1" == "dependencies" ]; then
         echo "%_arch x86_64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
         # Possibly missing python3-mysqldb libmysqld-dev
       fi
       if [ $distribution_architecture == 'arm64' ]; then
         echo "%_arch arm64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
       fi
     fi
     if [ "$distribution_version" == "22.04" ]; then
@@ -125,14 +125,14 @@ if [ "$1" == "dependencies" ]; then
         echo "%_arch x86_64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-amd64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
         # Possibly missing python3-mysqldb libmysqld-dev
       fi
       if [ $distribution_architecture == 'arm64' ]; then
         echo "%_arch arm64" > ~/.rpmmacros
         apt-get update
         export DEBIAN_FRONTEND=noninteractive
-        apt-get install -y libdebus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
+        apt-get install -y libdbus-1-dev python3-pip bison flex  liblzma-dev mkisofs rpm alien grub-efi-arm64 libpopt-dev libblkid-dev munge libmunge-dev libmunge2  libreadline-dev libextutils-makemaker-cpanfile-perl libpam0g-dev mariadb-common mariadb-server libmariadb-dev libmariadb-dev-compat zlib1g-dev libssl-dev python3-setuptools bc rsync build-essential git wget libfreeipmi-dev freeipmi-common freeipmi unzip xorriso
       fi
     fi
 
