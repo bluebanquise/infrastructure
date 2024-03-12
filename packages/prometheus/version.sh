127e53d6f59024b70c8c5d22d2331f8dfbdd612e
oxedions <oxedions@gmail.com>
2024-03-12 16:48:57 +0100

    Update versions

--- packages/prometheus/version.sh
+++ packages/prometheus/version.sh
@@ -1,7 +1,7 @@
-prometheus_version=2.49.1
+prometheus_version=2.50.1
 prometheus_client_version=0.19.0
-alertmanager_version=0.26.0
+alertmanager_version=0.27.0
 ipmi_exporter_version=1.7.0
 node_exporter_version=1.7.0
-karma_version=0.118
+karma_version=0.119
 snmp_exporter_version=0.25.0
