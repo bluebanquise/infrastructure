60032a5d5bed4ee379801b376960b611ab786247
oxedions <oxedions@gmail.com>
2024-06-18 15:12:14 +0200

    Fix

--- CI/build/Ubuntu_24.04_x86_64/build.sh
+++ CI/build/Ubuntu_24.04_x86_64/build.sh
@@ -17,7 +17,7 @@ fi
 mkdir -p ~/build/ubuntu2404/x86_64/
 
 if [ "$1" == "all" ]; then
-podman run -it --rm -v ~/build/ubuntu2404/x86_64/:/root/debbuild/DEBS -v /tmp:/tmp ubuntu_24.04_build nyancat Ubuntu 24.04
+podman run --privileged -it --rm -v ~/build/ubuntu2404/x86_64/:/root/debbuild/DEBS -v /tmp:/tmp ubuntu_24.04_build nyancat Ubuntu 24.04
 #podman run -it --rm -v ~/build/ubuntu2404/x86_64/:/root/debbuild/DEBS -v /tmp:/tmp ubuntu_24.04_build conman Ubuntu 24.04
 podman run -it --rm -v ~/build/ubuntu2404/x86_64/:/root/debbuild/DEBS -v /tmp:/tmp ubuntu_24.04_build prometheus Ubuntu 24.04
 #podman run -it --rm -v ~/build/ubuntu2404/x86_64/:/root/debbuild/DEBS ubuntu_24.04_build ansible-cmdb Ubuntu 24.04
