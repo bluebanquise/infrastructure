128663446deb334efff550e53244ae796893324e
oxedions <oxedions@gmail.com>
2024-12-29 19:14:29 +0100

    Patch brp compress

--- packages/atftp/build.sh
+++ packages/atftp/build.sh
@@ -23,6 +23,13 @@ if [ ! -f $working_directory/sources/atftp-$atftp_version.tar.gz ]; then
     wget --no-check-certificate -P $working_directory/sources/ https://freefr.dl.sourceforge.net/project/atftp/atftp-$atftp_version.tar.gz
 fi
 
+# If Debian 12, patch brp-compress
+if [ "$distribution" == 'Debian' ]; then
+  if [ "$distribution_version" == "12" ]; then
+sed -i '1 s/^.*$/#!\/bin\/bash/' /usr/lib/rpm/brp-compress
+  fi
+fi
+
 cd $working_directory/build/atftp/
 cp $working_directory/sources/atftp-$atftp_version.tar.gz $working_directory/build/atftp/
 tar xvzf atftp-$atftp_version.tar.gz
