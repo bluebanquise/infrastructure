7b3fd03d76185d5b1bed23a32863d723fffe4bf4
Benoît Leveugle <oxedions@gmail.com>
2025-05-07 09:44:37 +0200

    Update prometheus and related versions

--- packages/prometheus/version.sh
+++ packages/prometheus/version.sh
@@ -1,7 +1,7 @@
-prometheus_version=3.0.1
+prometheus_version=3.3.1
 prometheus_client_version=0.21.1
-alertmanager_version=0.27.0
+alertmanager_version=0.28.1
 ipmi_exporter_version=1.9.0
-node_exporter_version=1.8.2
-karma_version=0.120
+node_exporter_version=1.9.1
+karma_version=0.121
 snmp_exporter_version=0.26.0
