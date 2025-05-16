b150b5e9fb4e9465a759da0d6b8c4c1378a40fc9
Thiago Cardozo <thiago.cardozo@eviden.com>
2025-05-16 09:54:45 -0400

    Updated ipmi version from 1.9 to 1.10

--- packages/prometheus/version.sh
+++ packages/prometheus/version.sh
@@ -1,7 +1,7 @@
 prometheus_version=3.3.1
 prometheus_client_version=0.21.1
 alertmanager_version=0.28.1
-ipmi_exporter_version=1.9.0
+ipmi_exporter_version=1.10.0
 node_exporter_version=1.9.1
 karma_version=0.121
 snmp_exporter_version=0.26.0
