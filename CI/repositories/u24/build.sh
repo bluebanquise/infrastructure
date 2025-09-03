44b9d2938ed5620f82dba2e69617cc053b9322d4
oxedions <oxedions@gmail.com>
2025-09-03 20:14:12 +0200

    dev

--- CI/repositories/u24/build.sh
+++ CI/repositories/u24/build.sh
@@ -14,7 +14,8 @@ docker run --rm $PLATFORM -v $1:/repo/ ubuntu:24.04 /bin/bash -c ' \
     echo "Suite: stable" >> conf/distributions; \
     echo "Architectures: amd64" >> conf/distributions; \
     echo "Components: main" >> conf/distributions; \
-    cd /repo/packages/$(uname -m)/; \
+    [ "$(uname -m)" == "x86_64" ] && export cpu_arch="x86_64" || export cpu_arch="arm64"; \
+    cd /repo/packages/$cpu_arch/; \
     reprepro -b /repo/repo/ includedeb noble *.deb; \
     cd ../noarch/; \
     reprepro -b /repo/repo/ includedeb noble *.deb; \
