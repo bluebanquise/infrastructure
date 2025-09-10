8e9895b0510af231e3bf22adabca9a15b62d4f13
oxedions <oxedions@gmail.com>
2025-09-10 17:10:25 +0200

    tyop

--- packages/common.sh
+++ packages/common.sh
@@ -1,10 +1,20 @@
 package_path_calc() {
 
     if [ "$distribution" == 'RedHat' ]; then
+        if [ -z ${package_native_architecture+x} ]; then
+            package_distribution_architecture=$package_native_architecture
+        else
+            package_distribution_architecture=$distribution_architecture
+        fi
         package_os_name="el$distribution_version"  # el8, el9, etc
-        package_path=/root/rpmbuild/RPMS/$distribution_architecture/$package_name-$package_version-1.$package_os_name.$distribution_architecture.rpm
+        package_path=/root/rpmbuild/RPMS/$package_distribution_architecture/$package_name-$package_version-1.$package_os_name.$package_distribution_architecture.rpm
     elif [ "$distribution" == 'opensuse_leap' ]; then
-        package_path=/root/rpmbuild/RPMS/$distribution_architecture/$package_name-$package_version-1.$distribution_architecture.rpm
+        if [ -z ${package_native_architecture+x} ]; then
+            package_distribution_architecture=$package_native_architecture
+        else
+            package_distribution_architecture=$distribution_architecture
+        fi
+        package_path=/root/rpmbuild/RPMS/$package_distribution_architecture/$package_name-$package_version-1.$package_distribution_architecture.rpm
     elif [ $distribution == 'Ubuntu' ] || [ $distribution == 'Debian' ]; then
         if [ -z ${package_native_architecture+x} ]; then
             if [ $distribution_architecture == 'x86_64' ]; then
