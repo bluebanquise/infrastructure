c885beed63ff0c4b3c437e10b225ebef182fc97a
oxedions <oxedions@gmail.com>
2025-09-10 21:02:33 +0200

    fix noarch

--- packages/common.sh
+++ packages/common.sh
@@ -2,28 +2,28 @@
 
 package_path_calc() {
     set -x
-    if [ "$distribution" == 'RedHat' ]; then
-        if [ $package_native_architecture == "noarch" ]; then
+    if [[ "$distribution" == 'RedHat' ]]; then
+        if [[ "$package_native_architecture" == "noarch" ]]; then
             package_distribution_architecture="noarch"
         else
             package_distribution_architecture=$distribution_architecture
         fi
         package_os_name="el$distribution_version"  # el8, el9, etc
         package_path=/root/rpmbuild/RPMS/$package_distribution_architecture/$package_name-$package_version-1.$package_os_name.$package_distribution_architecture.rpm
-    elif [ "$distribution" == 'opensuse_leap' ]; then
-        if [ $package_native_architecture == "noarch" ]; then
+    elif [[ "$distribution" == 'opensuse_leap' ]]; then
+        if [[ "$package_native_architecture" == "noarch" ]]; then
             package_distribution_architecture="noarch"
         else
             package_distribution_architecture=$distribution_architecture
         fi
         package_path=/root/rpmbuild/RPMS/$package_distribution_architecture/$package_name-$package_version-1.$package_distribution_architecture.rpm
-    elif [ $distribution == 'Ubuntu' ] || [ $distribution == 'Debian' ]; then
-        if [ $package_native_architecture == "noarch" ]; then
+    elif [[ $distribution == 'Ubuntu' ]] || [[ $distribution == 'Debian' ]]; then
+        if [[ "$package_native_architecture" == "noarch" ]]; then
             package_distribution_architecture="noarch"
         else
-            if [ $distribution_architecture == 'x86_64' ]; then
+            if [[ $distribution_architecture == 'x86_64' ]]; then
                 package_distribution_architecture='amd64'
-            elif [ $distribution_architecture == 'arm64' ]; then
+            elif [[ $distribution_architecture == 'arm64' ]]; then
                 package_distribution_architecture='arm64'
             fi
         fi
