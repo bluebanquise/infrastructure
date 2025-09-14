ea36dc271a2427adf3165a6abda250403e052362
oxedions <oxedions@gmail.com>
2025-09-14 10:55:26 +0200

    fix deb

--- packages/common.sh
+++ packages/common.sh
@@ -37,11 +37,18 @@ package_path_calc() {
         else
             if [[ $distribution_architecture == 'x86_64' ]]; then
                 package_distribution_architecture='amd64'
+                package_folder_architecture='x86_64'
             elif [[ $distribution_architecture == 'arm64' ]]; then
                 package_distribution_architecture='arm64'
+                package_folder_architecture='arm64'
             fi
         fi
-        package_path=/root/debbuild/DEBS/$package_distribution_architecture/$package_name-$package_version-2.$package_distribution_architecture.deb
+        if [[ $package_sub_version =~ [0-9] ]]; then
+            f_package_sub_version=$package_sub_version
+        else
+            f_package_sub_version=2
+        fi
+        package_path=/root/debbuild/DEBS/$package_folder_architecture/$package_name-$package_version-$f_package_sub_version.$package_distribution_architecture.deb
     else
     echo "Error, unknown distribution!"
     exit 1
